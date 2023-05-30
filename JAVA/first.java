7f03b1fd8d8a1127fe07603429900019b52618ae
Aerogalaxystar <arindamnandy1823@gmail.com>
2023-05-30 18:25:39 +0530

    30th may test run

--- JAVA/first.java
+++ JAVA/first.java
@@ -2,6 +2,7 @@ class first{
 public static void  main(String args[]){
     System.out.println("Hello All");
     
-    System.out.println(" Git test Arindam");
+    System.out.println(" Git test Arindam ");
+    System.out.println("Test run on 30 may");
 }
 }
