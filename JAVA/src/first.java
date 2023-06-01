403fae8e0ec5c4247e2650a3ab6784a85e1fa18e
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-06-02 01:25:34 +0530

    Fixed IDE problem

--- JAVA/src/first.java
+++ JAVA/src/first.java
@@ -1,8 +1,7 @@
 class first{
 public static void  main(String args[]){
     System.out.println("Hello All");
-    
-    System.out.println(" Git test Arindam ");
+     System.out.println(" Git test Arindam ");
     System.out.println("Test run on 30 may");
     System.out.println("2nd test run on 30 may");
     System.out.println("3rd test run by Arindam,Abhishek and Pratham");
